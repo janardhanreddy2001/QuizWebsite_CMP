485218a3bf420fbc23ea24b1577ac07b02c4f42c
janardhanreddy2001 <jinkalajanardhanreddy77@gmail.com>
2025-06-30 15:38:34 +0530

    Update build.sh

--- CmpBackendGit/build.sh
+++ CmpBackendGit/build.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-set -e
+export JAVA_HOME=/opt/render/project/.jdk
+export PATH=$JAVA_HOME/bin:$PATH
 
-# Verify Java version
-java -version
+./mvnw clean package
 
-# Verify Maven (if using mvnw, skip installing Maven separately)
-if command -v mvn &> /dev/null
-then
-    echo "Maven is installed"
-else
-    echo "Maven is not installed"
-    exit 1
-fi
-
-# Make Maven wrapper executable
-chmod +x mvnw
-
-# Run Maven build, skipping tests for faster build
-./mvnw clean package -DskipTests
+java -jar target/*.jar
