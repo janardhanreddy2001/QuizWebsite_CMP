1fa5474b7b45ac39a2986dde1090df7006a667c6
janardhanreddy2001 <jinkalajanardhanreddy77@gmail.com>
2025-07-01 14:50:36 +0530

    Update ContentImplement.java

--- CmpBackendGit/src/main/java/com/example/demo/implement/ContentImplement.java
+++ CmpBackendGit/src/main/java/com/example/demo/implement/ContentImplement.java
@@ -47,7 +47,7 @@ public class ContentImplement implements ContentService {
 		contentRepository.save(content);
 		response.put("Status", "Seccuss");
 		response.put("Message", "create is successfully ");
-		 response.put("contentId", String.valueOf(content.getContentId()));
+	
 
 		return response;
 	}
