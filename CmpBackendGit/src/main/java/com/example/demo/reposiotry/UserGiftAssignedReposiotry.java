f4a91b1ffd0e74626206aae80c00d133bf28e056
janardhanreddy2001 <jinkalajanardhanreddy77@gmail.com>
2025-07-01 12:05:04 +0530

    Update UserGiftAssignedReposiotry.java

--- CmpBackendGit/src/main/java/com/example/demo/reposiotry/UserGiftAssignedReposiotry.java
+++ CmpBackendGit/src/main/java/com/example/demo/reposiotry/UserGiftAssignedReposiotry.java
@@ -1,4 +1,4 @@
-package com.example.demo.Reposiotry;
+package com.example.demo.reposiotry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
