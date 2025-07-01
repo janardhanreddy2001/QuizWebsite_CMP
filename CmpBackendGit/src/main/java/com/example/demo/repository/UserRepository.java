64caff2ba67c4befdf01c6af628ee6122dad446c
janardhanreddy2001 <jinkalajanardhanreddy77@gmail.com>
2025-07-01 13:46:42 +0530

    Update UserRepository.java

--- CmpBackendGit/src/main/java/com/example/demo/repository/UserRepository.java
+++ CmpBackendGit/src/main/java/com/example/demo/repository/UserRepository.java
@@ -8,11 +8,10 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.Entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer> {
 
-	int countByEmailId(String emailId);
-	Optional<User> findByEmailId(String emailId); 
+    int countByEmailId(String emailId);
 
-	
+    Optional<User> findByEmailId(String emailId);
 
 }
