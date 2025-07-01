ed84d15ef42159fb4bd4bb3508865f0db7690e54
janardhanreddy2001 <jinkalajanardhanreddy77@gmail.com>
2025-07-01 21:59:17 +0530

    cgage c

--- CmpBackendGit/src/main/java/com/example/demo/repository/CategoryRepository.java
+++ CmpBackendGit/src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.Entities.Category;
-
 @Repository
-public interface CategoryRepository extends MongoRepository<Category, String> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
     int countByCategoryTypeIgnoreCase(String categoryType);
 }
