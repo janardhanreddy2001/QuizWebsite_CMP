dd346cddc91225adf356e33c3eea883cb13c5295
janardhanreddy2001 <jinkalajanardhanreddy77@gmail.com>
2025-07-01 16:34:53 +0530

    Update build.sh

--- CmpBackendGit/build.sh
+++ CmpBackendGit/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Set Java 17 (only needed if Render doesn't auto-use Java 17)
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Move into the backend folder where pom.xml exists
+echo "==== Starting Maven Build in CmpBackendGit ===="
+
 cd CmpBackendGit
 
-# Build the Spring Boot application with Maven, skipping tests
-mvn clean package -DskipTests
+./mvnw clean install -DskipTests
