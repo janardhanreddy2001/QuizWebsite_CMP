97e65b4207bccd82c3e02ba685be78dc0f536e37
janardhanreddy2001 <jinkalajanardhanreddy77@gmail.com>
2025-07-02 06:47:30 +0530

    Update CategoryRepository.java

--- CmpBackendGit/src/main/java/com/example/demo/repository/CategoryRepository.java
+++ CmpBackendGit/src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository; 
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository; // ✅ JPA import
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Category;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     int countByCategoryTypeIgnoreCase(String categoryType);
